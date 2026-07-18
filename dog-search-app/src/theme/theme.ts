2dcd77707104ee6ffa14a9ba771c577cc7d68b17
pld208 <phwill2015@gmail.com>
2026-07-18 11:20:57 -0500

    basic nextJS setup

---
+++ dog-search-app/src/theme/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#085726",
+    },
+    secondary: {
+      main: "#9c27b0",
+    },
+    background: {
+      default: '#c8e6c9',
+    },
+  },
+});
+
+export default theme;
